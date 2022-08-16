8ab6f8249ed1e026fe4ef36c75e6ec526ef91178
jihan <jihanyasin04@gmail.com>
2022-08-16 16:32:14 +0200

    third commit

--- src/main/java/pages/HorizontalSliderPage.java
+++ src/main/java/pages/HorizontalSliderPage.java
@@ -1,17 +1,30 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 public class HorizontalSliderPage {
 
     private WebDriver driver;
-    private By slider = By.id("range");
+    private By slider = By.xpath("//input[@type='range']");
+    private By sliderValue = By.id("range");
 
     public HorizontalSliderPage(WebDriver driver)
     {
         this.driver = driver;
     }
 
-   // public void
+    public void setSliderValue (String value) //explain please
+    {
+        while (!getValue().equals(value))
+        {
+            driver.findElement(slider).sendKeys(Keys.ARROW_RIGHT);
+        }
+
+    }
+    public String getValue()
+    {
+        return driver.findElement(sliderValue).getText();
+    }
 }
--- src/test/java/slider/SliderTest.java
+++ src/test/java/slider/SliderTest.java
@@ -1,8 +1,20 @@
 package slider;
 
 import base.BaseTest;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class SliderTest extends BaseTest {
 
+    @Test
+    public void testSlider()
+    {
+        String value = "4";
+        var horizontalSliderPage = homePage.clickHorizontalSlider();
+        horizontalSliderPage.setSliderValue(value);
+        assertEquals(horizontalSliderPage.getValue(),value, "Slider value is incorrect");
+    }
 
 }
