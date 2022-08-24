58b0ce5ab9e36fb2f6fabc2bbdf9e81e16a7739a
Jihan Yasin <jyasin@dvt.co.za>
2022-08-24 13:44:36 +0200

    Chap13

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,17 +1,16 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.ITest;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 import pages.HomePage;
 
 import javax.print.DocFlavor;
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -37,11 +36,27 @@ public class BaseTest {
     {
         driver.get("https://the-internet.herokuapp.com/"); //launch browser
     }
+
     @AfterClass
     public  void teardown()
     {
         System.out.println(driver.getTitle());
         driver.quit();
+    }
+    @AfterMethod
+    public void recordFailure(ITestResult result)
+    {
+        if(ITestResult.FAILURE== result.getStatus()) {
+            //cast driver to Take Screenshot class
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/"+ result.getName()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 
